b1b294991f2ebf78fe4171bef369d020f71b43f3
Aishwarya Sharma <aishwarya.sharma>
2021-12-05 16:38:59 -0500

    attack for mlp

--- neural_networks.py
+++ neural_networks.py
@@ -6,11 +6,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.python.keras.backend import dtype
 from tensorflow.python.keras.layers.convolutional import Conv2D
-from tensorflow.python.keras.layers.pooling import MaxPool2D, MaxPooling2D
-from keras.optimizers import SGD
+#from keras.optimizers import SGD
 from sklearn.model_selection import KFold
-from keras.utils import to_categorical
+#from keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical
 from tensorflow.python.ops.init_ops_v2 import Initializer
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
 
 
 
@@ -55,16 +59,16 @@ def create_cnn():
     model.add(layers.Dense(10, activation='softmax'))
     
     # compile model
-    opt = SGD(lr=0.01, momentum=0.9)
-    model.compile(optimizer=opt,
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    #opt = SGD(lr=0.01, momentum=0.9)
+    #model.compile(optimizer=opt,
+    #              loss='categorical_crossentropy',
+    #              metrics=['accuracy'])
     
     return model
 
 
 def randomize(x, y):
-    permutation = np.random.permutation(y,shape[0])
+    permutation = np.random.permutation(y.shape[0])
     shuffled_x = x[permutation, :]
     shuffled_y = y[permutation, :]
     
@@ -97,6 +101,7 @@ def bias_variable(shape):
 batch_size = 64
 input_dim = 28
 units = 64
+feature_vector_length = 784
 output_size = 10  # labels are from 0 to 9
 
 # Build the RNN model
@@ -122,7 +127,17 @@ def build_model(allow_cudnn_kernel=True):
     return model
     
     
-    
+def build_mlp():
+    # Set the input shape
+    input_shape = (feature_vector_length,)
+    print(f'Feature shape: {input_shape}')
+
+    model = Sequential()
+    model.add(Dense(350, input_shape= input_shape, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(output_size, activation='softmax'))
+
+    return model
     
 
 
@@ -147,7 +162,7 @@ def evaluate_cnn_model(x_input, y_input, n_folds = 5):
                             batch_size=32,
                             validation_data=(testX, testY),
                             verbose=0)
-        
+
         # evaluate model
         _, acc = cnn_model.evaluate(testX, testY, verbose=0)
         #print('> %.3f' % (acc * 100.0))
@@ -163,9 +178,10 @@ def evaluate_cnn_model(x_input, y_input, n_folds = 5):
 def evaluate_rnn_model():
     
     (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+    print(x_test.shape)
     x_train, x_test = x_train / 255.0, x_test / 255.0
     sample, sample_label = x_train[0], y_train[0]
-    
+
     model = build_model(allow_cudnn_kernel=True)
 
     model.compile(loss=keras.losses.SparseCategoricalCrossentropy
@@ -179,14 +195,53 @@ def evaluate_rnn_model():
                        validation_data=(x_test, y_test), 
                        batch_size=batch_size, 
                        epochs=1)
+
     acc = history.history["accuracy"][0] * 100
     print(f"RNN Accuracy: {acc:.2f}%")
     
     return history
-    
-  
+
+
+def evaluate_mlp_model():
+    # Load the data
+    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+    # Reshape the data - MLPs do not understand such things as '2D'.
+    # Reshape to 28 x 28 pixels = 784 features
+    X_train = X_train.reshape(X_train.shape[0], feature_vector_length)
+    X_test = X_test.reshape(X_test.shape[0], feature_vector_length)
+
+    # Convert into greyscale
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+
+    # Convert target classes to categorical ones
+    Y_train = to_categorical(Y_train, output_size)
+    Y_test = to_categorical(Y_test, output_size)
+
+
+
+    # Create the model
+    model = build_mlp()
+
+    # Configure the model and start training
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, Y_train, epochs=10, batch_size=250, verbose=1, validation_split=0.2)
+
+    # Test the model after training
+    test_results = model.evaluate(X_test, Y_test, verbose=1)
+    #preds=model.predict(X_test)
+    #print(preds)
+    print(f'Test results - Loss: {test_results[0]*100} - Accuracy: {test_results[1]}*100%')
+
+    return model
+
+
 if __name__ == "__main__":
     trainX, trainY, testX, testY = load_mnist_data()
     trainX, testX = process_pixels(trainX, testX)
     acc = evaluate_rnn_model()
-    scores, histories = evaluate_cnn_model(trainX, trainY)
+   # scores, histories = evaluate_cnn_model(trainX, trainY)
+    history= evaluate_mlp_model()
